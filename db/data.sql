3fad70a802e3dd64dcb4a9d599012a84bf6291a7
ndominkiewicz <norbertd1410@gmail.com>
2026-04-19 16:59:45 +0200

    feat: add static data dump with user insert

---
+++ db/data.sql
@@ -0,0 +1,6 @@
+/* 
+    FILE:        data.sql
+    DESCRIPTION: Static data dump for testing purposes.
+*/
+
+INSERT INTO users (firstName, lastName, email, birthdate, password) VALUES ('Admin', 'LMS', '', '2000-05-15', 'passwd');
