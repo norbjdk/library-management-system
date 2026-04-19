773dc1856571b0305d4ce5d36ca988f6316cbb95
ndominkiewicz <norbertd1410@gmail.com>
2026-04-19 18:09:30 +0200

    feat: add role column with  default reader setup
    check in for reader, librarian and admin

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
 CREATE TABLE users (
-	id SERIAL PRIMARY KEY,
-	firstName VARCHAR(50) NOT NULL,
-	lastName VARCHAR(50) NOT NULL,
-	email VARCHAR(100) UNIQUE NOT NULL,
-	birthdate DATE NOT NULL,
-	createDate TIMESTAMP DEFAULT NOW()
-    password VARCHAR(255) NOT NULL
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    birthdate DATE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'reader' CHECK (role IN ('reader', 'librarian', 'admin')),
+    createDate TIMESTAMP DEFAULT NOW()
 );
