2dc7d465ceb6e05c7d3fc992f19b5299e9ee67ae
ndominkiewicz <norbertd1410@gmail.com>
2026-05-03 15:38:27 +0200

    feat: add sidebar with tailwind styling

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar';
+import { SidebarComponent } from './layout/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
